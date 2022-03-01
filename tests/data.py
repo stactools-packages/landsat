60342fc339544c918e74c13cd058b51e2ada391a
pjhartzell <preston.hartzell@gmail.com>
2022-03-01 12:39:07 -0500

    clean tests function names, add vcr to test_api_stac
    - vcr enables the test_api_stac functiont to operate offline even though it hits an "online" resource

--- tests/data.py
+++ tests/data.py
@@ -22,5 +22,7 @@ TEST_GEOMETRY_PATHS = {
         "data-files/assets2/LC08_L2SR_099120_20191129_20201016_02_T2_MTL.xml"),
     "stac_not_in_storage":
     test_data.get_path(
-        "data-files/assets5/LC08_L2SP_047027_20201204_20210313_02_T1_MTL.xml")
+        "data-files/assets5/LC08_L2SP_047027_20201204_20210313_02_T1_MTL.xml"),
+    "vcr_cassette":
+    test_data.get_path("fixtures/usgs_stac_api.yml")
 }
