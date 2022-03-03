22f599c8d37a29d7e4d10d7be6f1344e7e368f27
pjhartzell <preston.hartzell@gmail.com>
2022-03-03 18:05:02 -0500

    edit command documentation

--- src/stactools/landsat/commands.py
+++ src/stactools/landsat/commands.py
@@ -1,34 +1,31 @@
 import os
 
 import click
-import pystac
+from click import Command, Group
+from pystac import Item
 
 from stactools.landsat.stac import create_stac_item
 from stactools.landsat.utils import transform_stac_to_stac
 
 
-def create_landsat_command(cli):
-    """Creates a command group for working
-    with Landsat metadata from USGS' Collection 2
+def create_landsat_command(cli: Group) -> Command:
+    """Creates a command group for commands working with the USGS' Landsat
+    Collection 2 metadata.
     """
 
     @cli.group(
         'landsat',
         short_help=("Commands for working with Landsat Collection 2 metadata.")
     )
-    def landsat():
+    def landsat() -> None:
         pass
 
     @landsat.command(
         "create-item",
-        short_help="Create a STAC item from collection 2 scene metadata.")
-    @click.option("--level",
-                  type=click.Choice(['level-1', 'level-2'],
-                                    case_sensitive=False),
-                  default="level-2",
-                  show_default=True,
-                  help="Product level to process")
-    @click.option("--mtl", required=True, help="HREF to an MTL file.")
+        short_help="Create a STAC item from Collection 2 scene metadata.")
+    @click.option("--mtl",
+                  required=True,
+                  help="HREF to the source MTL metadata xml file")
     @click.option("--output",
                   required=True,
                   help="HREF of directory in which to write the item.")
@@ -36,17 +33,30 @@ def create_landsat_command(cli):
                   default=False,
                   show_default=True,
                   help="Use USGS STAC Item geometry")
-    def create_item_cmd(level: str, mtl: str, output: str,
-                        usgs_geometry: bool):
-        """Creates a STAC Item for a Landsat 8 C2 Level-2 scene's products.
+    def create_item_cmd(mtl: str, output: str, usgs_geometry: bool) -> None:
+        """\b
+        Creates a STAC Item for a Landsat Collection 2 scene based on metadata
+        from a Landsat MTL xml file. The following Landsat processing Levels and
+        sensors are supported:
+            Level-1
+                - Landsat 1-5 Multi Spectral Scanner (MSS)
+            Level-2
+                - Landat 4-5 Thematic Mapper (TM)
+                - Landsat 7 Enhanced Thematic Mapper (ETM)
+                - Landsat 8-9 Operational Land Imager - Thermal Infrared Sensor
+                  (OLI-TIRS)
 
-        All asset paths are based on the MTL path, as all assets are assumed to
-        reside in the same directory/blob prefix/etc.
+        All assets (COGs, metadata files) must reside in the same directory /
+        blob prefix / etc. as the MTL xml metadata file.
+
+        \b
+        Args:
+            mtl (str): HREF to the source MTL metadata xml file
+            output (str): Directory that will contain the STAC Item
+            usgs_geometry (bool, optional): Use the geometry from a USGS STAC
+                Item that resides in the same directory as the MTL xml file or
+                can be queried from the USGS STAC API.
         """
-        if level != 'level-2':
-            raise click.BadOptionUsage("level",
-                                       "Only level-2 currently implemented.")
-
         item = create_stac_item(mtl_xml_href=mtl,
                                 use_usgs_geometry=usgs_geometry)
         item.set_self_href(os.path.join(output, f'{item.id}.json'))
@@ -54,16 +64,28 @@ def create_landsat_command(cli):
 
     @landsat.command(
         "convert",
-        short_help="Convert a USGS STAC 0.7 Item to an updated STAC Item")
-    @click.option("--stac", "-s", required=True, help="Path to a STAC file.")
+        short_help="Convert a USGS STAC 0.7.0 Item to an updated STAC Item")
+    @click.option("-s",
+                  "--stac",
+                  required=True,
+                  help="HREF to the source STAC file.")
     @click.option(
-        "--enable-proj",
         "-p",
+        "--enable-proj",
         is_flag=True,
         help="Enable the proj extension. Requires access to blue band.")
-    @click.option("--dst", "-d", help="Output directory")
-    def convert_cmd(stac, enable_proj, dst):
-        in_item = pystac.Item.from_file(stac)
+    @click.option("-d", "--dst", help="Output directory")
+    def convert_cmd(stac: str, enable_proj: bool, dst: str) -> None:
+        """Converts a USGS STAC 0.7.0 Item to an updated STAC Item.
+
+        \b
+        Args:
+            stac (str): href to the source STAC file
+            dst (str): Directory that will contain the STAC Item
+            enable_proj (flag): Include the proj extension in the created STAC
+                Item
+        """
+        in_item = Item.from_file(stac)
         item = transform_stac_to_stac(in_item, enable_proj=enable_proj)
 
         item_path = os.path.join(dst, '{}.json'.format(item.id))
